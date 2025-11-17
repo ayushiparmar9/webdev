7b351eddd4d7a36b1efde25008ee58a0b8fd601c
ayushiparmar9 <ayushiparmar.7184@gmail.com>
2025-11-17 15:04:16 +0530

    websitebootstrap

---
+++ javascript/basicjs/index.js
@@ -0,0 +1,17 @@
+var a = 10;
+var a = 20;
+let b = 10;
+console.log(a, b);
+let ar =["Abc" ,1235 , 45.67 ,"a" , true]
+console.log(ar);
+ 
+console.warn("this is a warning")
+console.error("ye error hai")
+ alert("this is alert");
+
+
+if(confirm("are you sure")){
+    console.log("thanks");
+}else{
+    console.error("its ok");
+}
