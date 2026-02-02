f58a359fce8c1dfffb3973c0a2de1daf341d88b2
ayushiparmar9 <ayushiparmar.7184@gmail.com>
2026-02-02 20:28:12 +0530

    additemmodal

--- Cravings/client/src/components/restaurantDashboard/RestaurantMenu.jsx
+++ Cravings/client/src/components/restaurantDashboard/RestaurantMenu.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { useState } from 'react';
+import AddItemModal from './models/AddItemModal';
 
 const RestaurantMenu = () => {
+  
+  const [isAddItemModalOpen ,setIsAddItemModalOpen] = useState(false);
+
   return (
-    <div>RestaurantMenu</div>
+    <>
+    <div className='flex justify-between  shadow rounded-3xl p-6'>
+    <h2 className='text-red-600 font-bold'>Menu</h2>
+    <button className='bg-yellow-400 p-2 rounded-2xl text-amber-100'onClick={() => setIsAddItemModalOpen(true)} >Add item</button>
+    </div>
+    
+    {isAddItemModalOpen && (
+            <AddItemModal
+              onClose={() => setIsAddItemModalOpen(false)}/>)}
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </>
   )
 }
 
---
+++ Cravings/server/src/models/menuSchema.js
@@ -0,0 +1,61 @@
+import mongoose, { Mongoose } from 'mongoose';
+const menuSchema = mongoose.Schema({
+   restaurantID:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'user',
+    required:true,
+
+   } ,
+   dishName:{
+    type:String,
+    required:true,
+   },
+   cuisine:{
+    type:String,
+    required:true,
+   },
+   type:{
+    type:String,
+    enum:['non-veg','vegan','egg','jain','gluten-free','conatins-nuts','dairy'],
+    required:true,
+   },
+   description:{
+    
+    type:String,
+    required:true,
+   
+   },
+   price:{
+    type:String,
+    required:true,
+   },
+   availability:{
+    type:String,
+    required:true,
+    enum:['available','unavailable','removed'],
+    default:'available'
+   },
+   image:{
+    type:[
+        {
+            url:{
+    type:String,
+    required:true,
+   },
+   publicID:{
+    type:String,
+    required:true,
+   }
+
+        }
+    ],
+
+required:true,
+    
+   }
+
+},
+ { timestamps: true },
+)
+const Menu = mongoose.model("menu", menuSchema);
+export default Menu;
